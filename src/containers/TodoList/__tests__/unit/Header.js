289fdc4e25f182285abb411e6f00a8a94ed3a1e7
张蕾 <1lei.zhang@zhaopin.com.cn>
2020-03-24 15:12:03 +0800

    refator:优化代码

--- src/containers/TodoList/__tests__/unit/Header.js
+++ src/containers/TodoList/__tests__/unit/Header.js
@@ -1,5 +1,6 @@
 import { shallowMount } from '@vue/test-utils'
 import Header from '../../components/Header.vue'
+import { findTestWrapper } from '../../../../testUtils'
 
 it('Header 样式发生改变，做提示', () => {
   const wrapper = shallowMount(Header)
@@ -7,7 +8,7 @@ it('Header 样式发生改变，做提示', () => {
 })
 it('组件渲染正常', () => {
   const wrapper = shallowMount(Header)
-  const input = wrapper.find('[data-test="input"]')
+  const input = findTestWrapper(wrapper, 'input')
   expect(input.exists()).toBe(true)
 })
 it('Header中input框初始内容为空', () => {
@@ -17,7 +18,7 @@ it('Header中input框初始内容为空', () => {
 })
 it('Header中input发生变化，数据应该跟着变', () => {
   const wrapper = shallowMount(Header)
-  const input = wrapper.find('[data-test="input"]')
+  const input = findTestWrapper(wrapper, 'input')
   input.setValue('hello')
   const inputValue = wrapper.vm.$data.inputValue
   expect(inputValue).toBe('hello')
@@ -25,21 +26,21 @@ it('Header中input发生变化，数据应该跟着变', () => {
 
 it('Header中input框输入回车，无内容时，无反应', () => {
   const wrapper = shallowMount(Header)
-  const input = wrapper.find('[data-test="input"]')
+  const input = findTestWrapper(wrapper, 'input')
   input.setValue('')
   input.trigger('keyup.enter')
   expect(wrapper.emitted().add).toBeFalsy()
 })
 it('Header中input框输入回车，有内容时，向外触发事件', () => {
   const wrapper = shallowMount(Header)
-  const input = wrapper.find('[data-test="input"]')
+  const input = findTestWrapper(wrapper, 'input')
   input.setValue('test')
   input.trigger('keyup.enter')
   expect(wrapper.emitted().add).toBeTruthy()
 })
 it('Header中input框输入回车，有内容时，向外触发事件，同时清空inputValue', () => {
   const wrapper = shallowMount(Header)
-  const input = wrapper.find('[data-test="input"]')
+  const input = findTestWrapper(wrapper, 'input')
   input.setValue('test')
   input.trigger('keyup.enter')
   expect(wrapper.vm.$data.inputValue).toBe('')
