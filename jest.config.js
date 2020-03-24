bdee95afc081e605cdebaf3b906286d2c2a0ca14
张蕾 <1lei.zhang@zhaopin.com.cn>
2020-03-25 07:22:16 +0800

    feat:覆盖率配置文件

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
-  testMatch: ['**/__tests__/**/*.{j,t}s?(x)']
+  testMatch: ['**/__tests__/**/*.{j,t}s?(x)'],
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{js,vue}', '!**/node_modules/**']
 }
